17ad844934f04d240b1e64421084525ef08e18f0
ayan00dey <mr.dey.ayanava@gmail.com>
2025-03-23 21:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+function handleHover(event){
+	squares.forEach(square =>{
+		if(square !== event.target){
+			square.style.backgroundColor = "#6F4E37";
+		}
+	})
+}
+
+function resetColor(event){
+	squares.forEach(square =>{
+		
+			square.style.backgroundColor = "#E6E6FA";
+		
+	})
+}
+
+squares.forEach(square =>{
+	square.addEventListener("mouseenter",handleHover);
+	square.addEventListener("mouseleave",resetColor);
+})
