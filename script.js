0693b03a55a78c9377a7a0adece0f2e7f417c505
ayan00dey <mr.dey.ayanava@gmail.com>
2025-03-28 11:16:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,13 @@
 const squares = document.querySelectorAll(".square");
 
 // Function to change other squares to Coffee color on hover
-squares.forEach((square, index) => {
-    square.addEventListener("mouseenter", () => {
-        squares.forEach((s, i) => {
-            if (i !== index) s.style.backgroundColor = "#6F4E37"; // Coffee
-        });
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        squares.forEach(s => s.style.backgroundColor = s === square ? "#E6E6FA" : "#6F4E37");
     });
 
     // Reset colors on mouseout
-    square.addEventListener("mouseleave", () => {
+    square.addEventListener("mouseout", () => {
         squares.forEach((s) => s.style.backgroundColor = "#E6E6FA"); // Lavender
     });
 });
